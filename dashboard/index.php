0fbe30496f12a050a39c4c566be76b1544f0cea4
Trung <91406241+mniac810@users.noreply.github.com>
2024-06-30 01:03:49 +0700

    Update index.php

--- dashboard/index.php
+++ dashboard/index.php
@@ -3,6 +3,9 @@
     include('../include/function.php');
     require_once '../include/config_session.inc.php';
     require_once '../include/databaseHandler.inc.php';
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
     if (isset($_SESSION['user_username'])) {
 ?>
 
